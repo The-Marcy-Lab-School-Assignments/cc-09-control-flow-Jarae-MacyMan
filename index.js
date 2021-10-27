9978522cbb630cb94a2a3302bac59013b24bf1cd
Jarae-MacyMan <sjaraebr@gmail.com>
2021-10-27 15:21:06 +0000

    complete

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(int){
+   return ( int > 4 && int < 11) ? true : false;
 }
+console.log( betweenFiveAndTen(6));
+
+
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(num){
+    return ( num > 9 && num < 31) ? true : false;
 }
 
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(int){
+    return ( int > 29 && int < 51) ? true : false;
 }
 
 
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(int){
+     (( int > -1 && int < 31) || (int > 49 && int < 71)) ? true :  false;
 }
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(int){
+   ( ( int > -1 && int < 21) || (int > 49 && int < 61)) ? true :  false;
 }
 
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(int){
+    ( (int > -1 && int < 11)||(int > 19 && int < 31)) ? true :  false;
 }
+
